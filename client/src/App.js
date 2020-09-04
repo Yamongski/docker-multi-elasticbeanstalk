2b725a11779e4d22f7fe688952d708100b382ff5
Abryan Manalansang <abryan@bbtdigital.com>
2020-09-04 09:52:27 +0800

    app js feature edit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React, custom title!</h1>
+            <h1 className="App-title">Calculator!!!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
